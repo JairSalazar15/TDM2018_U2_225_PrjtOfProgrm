881c5b5a3776f74de58f92c9436b15a972c77318
Jair <irjaslazarna@ittepic.edu.mx>
2018-03-13 13:42:26 -0700

    Initial commit

---
+++ app/src/main/java/tdm2018/ittepic/edu/tdm2018_u2_225_prjtofprogrm/AgregarActividad.java
@@ -0,0 +1,13 @@
+package tdm2018.ittepic.edu.tdm2018_u2_225_prjtofprogrm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_actividad);
+    }
+}
